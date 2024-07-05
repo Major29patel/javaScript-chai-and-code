e7a6ef91877fe12c12dabdee85fe4faa44d090d7
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-05 18:08:40 +0000

    container confid done

--- basics/day1.js
+++ basics/day1.js
@@ -1,1 +1,14 @@
 console.log("Rahul")
+const rahulRollNumber = 211090
+let rahulemail = "patel2921rahul@gmail.com"
+var rahulpassword = "Patel0811@"
+rahulCity = "Rewa"
+let rahulstate
+
+ // rahulRollNumber = 26 // const cant be changed.
+
+ rahulemail = "patel2921rahul@outlook.com"
+ rahulpassword = "Patel3112@"
+ rahulCity = "Bhopal"
+
+ console.table([rahulRollNumber , rahulemail, rahulRollNumber , rahulpassword, rahulCity, rahulstate])

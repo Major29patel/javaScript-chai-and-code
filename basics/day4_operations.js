99a809441f7a7d7e435265119fbd5e1bc5dcc1c0
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-06 18:04:57 +0000

    operations

---
+++ basics/day4_operations.js
@@ -0,0 +1,24 @@
+let val = 3;
+let negativeval = -val;
+console.log(negativeval);
+let str1 ="hello"
+let str2 ="rahul"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+
+console.log("1"+2+3);
+console.log(1+2+"2");
+console.log((3 +4) *5 % 3);
+
+console.log(+true)
+
+let num1 = num2 = num3 =4
+
+let getcounter = 100;
+++getcounter;
+console.log(getcounter)
+ 

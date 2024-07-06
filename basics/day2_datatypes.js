5f5b76a6372421228c011ac75764f6c0be8dc9a9
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-06 16:17:45 +0000

    datatypes

---
+++ basics/day2_datatypes.js
@@ -0,0 +1,13 @@
+// number => 2 to power of 53
+// bigInt 
+// string
+// boolean
+// null =? standlalone value
+// undefined
+// symbol => unique
+
+
+// Object
+console.log(typeof null)
+console.log(typeof undefined)
+

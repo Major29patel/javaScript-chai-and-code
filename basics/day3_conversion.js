50e1d29eb522b6f158487acff3fdf808e74685a1
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-06 17:11:49 +0000

    conversion

---
+++ basics/day3_conversion.js
@@ -0,0 +1,18 @@
+let score = undefined
+
+
+console.log(typeof score)
+
+let valNum = Number (score)
+console.log(typeof valNum)
+console.log(valNum)
+
+//"33" => 33
+// "33a" => NaN
+// true = 1 ; false =0;
+
+let isloggedin = NaN
+
+let booleanIsLoggedIn = Boolean(isloggedin);
+console.log(isloggedin);
+console.log(booleanIsLoggedIn)

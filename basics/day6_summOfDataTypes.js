b000de857b3050ba24a30faeab8f2560f8bf0b3a
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-11 19:11:10 +0000

    summary

---
+++ basics/day6_summOfDataTypes.js
@@ -0,0 +1,37 @@
+//PRIMITIVE 
+
+// 7 TYPES : String , Number , Boolean , null , undefined , Symbol 
+
+
+
+//REFERENCE || NON-PRIMITIVE
+// Arrays , Objects , Fuctions
+
+const score = 100
+const scoreval    = 100.3
+
+const loggedIn  = false
+const outside = null
+ 
+let userEmail
+
+const  id =  Symbol('123')
+const anoId = Symbol('123')
+
+console.log(id === anoId)
+
+const biggggnumber   = 342543632645623652365n
+
+const heros =["batsman", "ironman","shaktiman"]
+letmyObj ={
+    name: "rahul",
+    ae: 22
+}
+
+const    myFunctio= function(){
+    console.log("hhello")
+}
+
+console.log(biggggnumber)
+console.log(typeof(outside))
+console.log(typeof(myFunctio))

6ecc72a87e6c5048fb49708f4bb3eeab1d6d95f2
Rahul Patel <104488229+Major29patel@users.noreply.github.com>
2024-07-11 18:15:49 +0000

    comparisons

---
+++ basics/day5.js
@@ -0,0 +1,23 @@
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+
+// let str1="hello"
+// let str2 = "rahul"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 != 1)
+
+
+console.log("2" > 1)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log(undefined < 0)
+
+// ===
+console.log("2" === 2);
